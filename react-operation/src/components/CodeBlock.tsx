a5d8c5fa4e44a2ff5538c399613dbf3b14f81f65
yooochae <yyoochae@gmail.com>
2026-07-31 23:33:25 +0900

    fix: 코드블록을 키보드로 스크롤할 수 있도록 tabindex 추가
    가로 스크롤이 생기는 영역인데 포커스를 받을 수 없어,
    키보드만 쓰는 사용자는 잘린 코드를 끝까지 볼 수 없었음.

--- react-operation/src/components/CodeBlock.tsx
+++ react-operation/src/components/CodeBlock.tsx
@@ -12,7 +12,8 @@ export function CodeBlock({ code, caption }: { code: string; caption: string }) 
   return (
     <figure>
       <figcaption className="visually-hidden">{caption}</figcaption>
-      <pre className="code-block">
+      {/* 가로 스크롤이 생기는 영역이므로 키보드로도 스크롤할 수 있어야 한다 */}
+      <pre className="code-block" tabIndex={0}>
         <code>{code}</code>
         <button
           type="button"
